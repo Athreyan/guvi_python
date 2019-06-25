f06899a849aea14583510750a26e2b5278d2a5c9
athreyan <42887628+Athreyan@users.noreply.github.com>
2019-06-25 14:20:05 +0530

    Delete greatest.py

--- greatest.py
+++
@@ -1,9 +0,0 @@
-value1=int(input("Enter the first value :"))
-value2=int(input("Enter the second value :"))
-value3=int(input("Enter the third value :"))
-if(value1>value2 and value1>value3):
-	print(value1)
-elif(value2>value3):
-	print(value2)
-else:
-	print(value3)
