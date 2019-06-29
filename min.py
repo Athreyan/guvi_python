508353e6b70b49b24c59e62d21bd9f12975b575a
athreyan <42887628+Athreyan@users.noreply.github.com>
2019-06-29 19:17:49 +0530

    print min in list

---
+++ min.py
@@ -0,0 +1,12 @@
+
+num=int(input())
+dd=list(map(int,input().split()[:num]))
+minval=dd[0]
+for i in dd:
+    if(minval>i):
+        minval=i
+print(minval)
+    
+    
+#sor=sorted(dd,reverse=False)
+#print(sor[num-1])
