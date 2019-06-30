f2eb423f6f040942cccce6966350d7dc75146b4d
athreyan <42887628+Athreyan@users.noreply.github.com>
2019-06-30 09:01:17 +0530

    sort using quicksort

---
+++ quick.py
@@ -0,0 +1,34 @@
+
+def quicksort(alist, start, end):
+   
+    if end - start > 1:
+        p = partition(alist, start, end)
+        quicksort(alist, start, p)
+        quicksort(alist, p + 1, end)
+ 
+ 
+def partition(alist, start, end):
+    pivot = alist[start]
+    i = start + 1
+    j = end - 1
+ 
+    while True:
+        while (i <= j and alist[i] <= pivot):
+            i = i + 1
+        while (i <= j and alist[j] >= pivot):
+            j = j - 1
+ 
+        if i <= j:
+            alist[i], alist[j] = alist[j], alist[i]
+        else:
+            alist[start], alist[j] = alist[j], alist[start]
+            return j
+ 
+ 
+num=int(input())
+alist = list(map(int,input().split()[:num])) 
+
+quicksort(alist, 0, len(alist))
+for i in range(len(alist)):
+        print(alist[i] , end=" ")
+
