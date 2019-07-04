7bbbf3fc6d4b9fedc69aabbb7b10135fe0bc313b
athreyan <42887628+Athreyan@users.noreply.github.com>
2019-07-04 10:27:34 +0530

    Update num_wrd.py

--- num_wrd.py
+++ num_wrd.py
@@ -1,7 +1,7 @@
 
 def int_to_en(num):
-    d = { 0 : 'zero', 1 : 'one', 2 : 'two', 3 : 'three', 4 : 'four', 5 : 'five',
-          6 : 'six', 7 : 'seven', 8 : 'eight', 9 : 'nine', 10 : 'ten'}
+    d = { 0 : 'Zero', 1 : 'One', 2 : 'Two', 3 : 'Three', 4 : 'Four', 5 : 'Five',
+          6 : 'Six', 7 : 'Seven', 8 : 'Eight', 9 : 'Nine', 10 : 'Ten'}
     return d[num]
 num=int(input())
 print(int_to_en(num))
