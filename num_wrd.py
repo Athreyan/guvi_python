5b019e77dc896f5ba2c202bee3048b04cc83bb22
athreyan <42887628+Athreyan@users.noreply.github.com>
2019-07-04 10:23:29 +0530

    number to word conversion

---
+++ num_wrd.py
@@ -0,0 +1,7 @@
+
+def int_to_en(num):
+    d = { 0 : 'zero', 1 : 'one', 2 : 'two', 3 : 'three', 4 : 'four', 5 : 'five',
+          6 : 'six', 7 : 'seven', 8 : 'eight', 9 : 'nine', 10 : 'ten'}
+    return d[num]
+num=int(input())
+print(int_to_en(num))
