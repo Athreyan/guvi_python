1dbcd398a5f8f88241ce6888c79197e40858cbea
athreyan <42887628+Athreyan@users.noreply.github.com>
2019-07-05 20:11:08 +0530

    check presence of alphabet and number

--- alpha_numer.py
+++ alpha_numer.py
@@ -1,5 +1,7 @@
 val=input()
-if val.isalpha() and val.isnumeric():
-    print("No")
+for v in range(0,len(val)):
+    
+    if (val[v].isalpha() and val[v].isnumeric()):
+        print("No")
 else:
-    print("Yes")
+        print("Yes")
