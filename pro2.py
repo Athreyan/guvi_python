ede8647d96c44285a7d27a6d542c4cf9a13a0205
athreyan <42887628+Athreyan@users.noreply.github.com>
2019-08-12 18:11:05 +0530

    Update pro2.py

--- pro2.py
+++ pro2.py
@@ -4,9 +4,9 @@ from itertools import combinations
 num1,num2=map(int,input().split())
 le=len(str(num1))
 l=list(combinations(str(num1),le-num2))
-print(l)
+#print(l)
 l=(sorted(l))
-print(l)
+#print(l)
 y="".join(l[0])
 print(y)
 
