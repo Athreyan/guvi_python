1aa4cf5c0a1cc809c290b13fc4d458614812f13f
athreyan <42887628+Athreyan@users.noreply.github.com>
2019-08-12 18:09:02 +0530

    COmbination of least value

---
+++ pro2.py
@@ -0,0 +1,12 @@
+
+    
+from itertools import combinations
+num1,num2=map(int,input().split())
+le=len(str(num1))
+l=list(combinations(str(num1),le-num2))
+print(l)
+l=(sorted(l))
+print(l)
+y="".join(l[0])
+print(y)
+
